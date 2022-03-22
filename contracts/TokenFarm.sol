198789b7b172fd835360490543793c4d73f1478e
Qamarul Arifin <qabd88@icloud.com>
2022-03-22 13:37:32 +0800

    update brownie config and tokenfarm

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -3,11 +3,13 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract TokenFarm is Ownable {
     // need mapping of token address -> staker address -> amount
     mapping(address => mapping(address => uint256)) public stakingBalance;
     mapping(address => uint256) public uniqueTokensStaked;
+    mapping(address => address) public tokenPriceFeedMapping;
     address[] public stakers; //this list is so that we can capture the list of stakers for that particular token
 
     //stake token
@@ -16,6 +18,19 @@ contract TokenFarm is Ownable {
     //add allowed token
     //getEthValue
     address[] public allowedTokens; //this is a list of allowed tokens to be staked
+    IERC20 public dappToken;
+
+    constructor(address _dappTokenAddress) public {
+        dappToken = IERC20(_dappTokenAddress); //defining dapptoken variable as a dapptoken token from dapptoken.sol
+    }
+
+    //function to get the price feed contract
+    function setPriceFeedContract(address _token, address _priceFeed)
+        public
+        onlyOwner
+    {
+        tokenPriceFeedMapping[_token] = _priceFeed;
+    }
 
     //function that will issue token as a reward for staking. for 1 ether give 1 dapptoken, for dai, need to covnert to eth first and then provide 1 dapptoken
     function issueTokens() public onlyOwner {
@@ -25,8 +40,59 @@ contract TokenFarm is Ownable {
             stakersIndex++
         ) {
             address recipient = stakers[stakersIndex];
+            address userTotalValue = getUserTotalValue(recipient);
+
             //send them token reward based on total value locked
+            dappToken.transfer(recipient, userTotalValue); //token farm contract is the contract holding the dapptoken
         }
+    }
+
+    //function to get UserTotalValue,  user will get the value instead of protocol issuing, more gas efficient.  totalvalue across all owned tokens by user
+    function getUserTotalValue(address _user) public view returns (uint256) {
+        uint256 totalValue = 0;
+        require(uniqueTokensStaked[_user] > 0, "no staked tokens");
+        for (
+            uint256 allowedTokensIndex = 0;
+            allowedTokensIndex < allowedTokens.length;
+            allowedTokensIndex++
+        ) {
+            totalValue =
+                totalValue +
+                getUserSingleTokenValue(
+                    _user,
+                    allowedTokens[allowedTokensIndex]
+                );
+        }
+        return totalValue; //this is the total value of all the tokens owned by the user
+    }
+
+    //function to get a user single token value
+    function getUserSingleTokenValue(address _user, address _token)
+        public
+        view
+        returns (uint256)
+    {
+        if (uniqueTokensStaked[_user] <= 0) {
+            return 0;
+        }
+        (uint256 price, uint256 decimals) = getTokenValue(_token); //need price of the token
+        return ((stakingBalance[_token][_user] * price) / (10**decimals));
+    }
+
+    //function to get the price of the token
+    function getTokenValue(address _token)
+        public
+        view
+        returns (uint256, uint256)
+    {
+        //will need to use chainlink pricefeeds
+        address priceFeedAddress = tokenPriceFeedMapping[_token];
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            priceFeedAddress
+        ); //this is defining the priceFeed contract by utilizing the interface
+        (, int256 price, , , , ) = priceFeed.latestRoundData();
+        uint256 decimals = priceFeed.decimals();
+        return (uint256(price), decimals);
     }
 
     //function takes in amount to stake and the token contract address
