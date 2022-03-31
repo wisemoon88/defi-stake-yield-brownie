d33015c525387f17107dbf8ed7cf6fa33600447f
QAMARUL ARIFIN BIN ABD MANAN <qabd88@icloud.com>
2022-03-31 08:21:42 +0800

    update FrontEnd

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { ChainId, DAppProvider } from "@usedapp/core"
+import { Header } from "./components/Header"
+import { Container } from "@material-ui/core"
 
 function App() {
   return (
     <DAppProvider config={{
       supportedChains: [ChainId.Kovan, ChainId.Rinkeby]
     }}>
-
-      <div className="App">
-        Hi!
-      </div>
+      <Header />
+      <Container maxWidth="md">
+        <div>
+          Hi!
+        </div>
+      </Container>
     </DAppProvider>
 
   );
